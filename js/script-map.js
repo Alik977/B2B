c347d05304753d839ab01841ef006aaab60a4756
Alik977 <alik.hakobyan.sh@gmail.com>
2026-01-07 00:16:33 +0400

    processmap

---
+++ js/script-map.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const map = L.map("map").setView([40.22744327298755, 44.47278672260408], 13);
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "© OpenStreetMap",
+  }).addTo(map);
+
+  L.marker([40.22744327298755, 44.47278672260408])
+    .addTo(map)
+    .bindPopup("Մեր գտնվելու վայրը")
+    .openPopup();
+});
