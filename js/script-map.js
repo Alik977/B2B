271de05dc9b690f4f2bfabeedb66930e3c65cd76
Alik977 <alik.hakobyan.sh@gmail.com>
2026-01-28 01:29:47 +0400

    scrollup

--- js/script-map.js
+++ js/script-map.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const map = L.map("map").setView([40.22744327298755, 44.47278672260408], 13);
+  const map = L.map("map").setView([40.22727243656217, 44.472775107071406], 13);
 
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "© OpenStreetMap",
