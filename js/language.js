d9c9779bcb7e2eb31a27b148c90ea8986f7e5662
Alik977 <alik.hakobyan.sh@gmail.com>
2026-02-05 02:07:00 +0400

    language change

--- js/language.js
+++ js/language.js
@@ -31,6 +31,26 @@ menu_paragraph: "Մենք ստեղծագործական դիզայնի գործա
      menu_click: "Подробнее",
      menu_paragraph: "Мы — креативное дизайн-агентство, которое сотрудничает с компаниями для формирования их стратегического брендинга и визуальных коммуникаций. Наша команда сочетает креативное мышление с бизнес-логикой для создания значимых и эффективных решений. Мы верим, что хороший дизайн укрепляет доверие, связывает бренды с людьми и ускоряет рост.."
   },
+  fr: {
+    menu_home: "MAISON",
+    menu_about: "À PROPOS DE NOUS",
+    menu_services: "SERVICES",
+    menu_portfolio: "PORTEFEUILLE",
+    menu_blog: "BLOGUE",
+    menu_contact: "CONTACTS",
+     menu_click: "Plus",
+     menu_paragraph: "Nous sommes une agence de design créatif qui accompagne les entreprises dans le développement de leur stratégie de marque et de leur communication visuelle. Notre équipe allie créativité et sens des affaires pour concevoir des solutions pertinentes et efficaces. Nous sommes convaincus qu'un design de qualité renforce la confiance, crée du lien entre les marques et leur public, et accélère la croissance."
+  },
+  ger: {
+    menu_home: "STARTSEITE",
+    menu_about: "ÜBER UNS",
+    menu_services: "DIENSTLEISTUNGEN",
+    menu_portfolio: "PORTFOLIO",
+    menu_blog: "BLOG",
+    menu_contact: "KONTAKT",
+     menu_click: "Mehr",
+     menu_paragraph: "Wir sind eine Kreativagentur, die Unternehmen bei der Gestaltung ihrer strategischen Markenbildung und visuellen Kommunikation unterstützt. Unser Team verbindet kreatives Denken mit betriebswirtschaftlichem Know-how, um sinnvolle und effektive Lösungen zu entwickeln. Wir sind überzeugt, dass gutes Design Vertrauen schafft, Marken mit Menschen verbindet und Wachstum beschleunigt.."
+  },
 };
 
 langSelected.addEventListener("click", () => {
@@ -71,6 +91,10 @@ if (savedLang === "en") {
   setLanguage("en", "EN", "./pictures/flag/usa.png");
 } else if (savedLang === "ru") {
   setLanguage("ru", "RU", "./pictures/flag/rus.png");
+} else if (savedLang === "fr") {
+  setLanguage("fr", "FR", "./pictures/flag/france.png");
+} else if (savedLang === "ger") {
+  setLanguage("ger", "Ger", "./pictures/flag/german.png");
 } else {
   setLanguage("hy", "Հայ", "./pictures/flag/arm.png");
 }
